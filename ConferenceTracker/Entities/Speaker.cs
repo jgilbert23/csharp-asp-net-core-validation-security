e03b2db60d69b07464195d8f05a741da4582231e
Jeff Gilbert <Jeff.Gilbert@wolterskluwer.com>
2022-04-15 09:09:34 -0600

    Added validation to Speaker.

--- ConferenceTracker/Entities/Speaker.cs
+++ ConferenceTracker/Entities/Speaker.cs
@@ -6,15 +6,24 @@ namespace ConferenceTracker.Entities
 {
     public class Speaker
     {
+        [Required]
         public int Id { get; set; }
         [Display(Name = "First name")]
+        [Required]
+        [DataType (DataType.Text)]
         public string FirstName { get; set; }
         [Display(Name = "Last name")]
+        [Required]
+        [DataType(DataType.Text)]
         public string LastName { get; set; }
+        [Required]
+        [DataType (DataType.MultilineText)]
         public string Description { get; set; }
         [Display(Name = "Email Address")]
+        [DataType (DataType.EmailAddress)]
         public string EmailAddress { get; set; }
         [Display(Name = "Phone Number")]
+        [DataType(DataType.PhoneNumber)]
         public string PhoneNumber { get; set; }
         public bool IsStaff { get; set; }
     }
