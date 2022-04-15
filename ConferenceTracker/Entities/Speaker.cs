7ee1c535b3dc0f7c01ca53ce09b6257eff64d483
Jeff Gilbert <Jeff.Gilbert@wolterskluwer.com>
2022-04-15 09:56:43 -0600

    Updated for exercise 1.

--- ConferenceTracker/Entities/Speaker.cs
+++ ConferenceTracker/Entities/Speaker.cs
@@ -4,20 +4,23 @@ using System.ComponentModel.DataAnnotations;
 
 namespace ConferenceTracker.Entities
 {
-    public class Speaker
+    public class Speaker : IValidatableObject
     {
         [Required]
         public int Id { get; set; }
         [Display(Name = "First name")]
         [Required]
         [DataType (DataType.Text)]
+        [StringLength(maximumLength:100, MinimumLength = 2)]
         public string FirstName { get; set; }
         [Display(Name = "Last name")]
         [Required]
         [DataType(DataType.Text)]
+        [StringLength(maximumLength: 100, MinimumLength = 2)]
         public string LastName { get; set; }
         [Required]
         [DataType (DataType.MultilineText)]
+        [StringLength(maximumLength: 500, MinimumLength = 10)]
         public string Description { get; set; }
         [Display(Name = "Email Address")]
         [DataType (DataType.EmailAddress)]
@@ -26,5 +29,15 @@ namespace ConferenceTracker.Entities
         [DataType(DataType.PhoneNumber)]
         public string PhoneNumber { get; set; }
         public bool IsStaff { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            var result = new List<ValidationResult>();
+            if (EmailAddress != null && EmailAddress.EndsWith("TechnologyLiveConference.com", StringComparison.InvariantCultureIgnoreCase))
+            {
+                result.Add(new ValidationResult("Technology Live Conference staff should not use their conference email addresses."));
+            }
+            return result;
+        }
     }
 }
